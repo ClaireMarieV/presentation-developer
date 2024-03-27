7be7de09a3be1408384897dd1e8b5793a4ba7824
Valentin Manceaux-Panot <valentin@mcxp.io>
2024-03-27 14:55:14 +0100

    Fixed the last slide

--- src/app/presentation/9/page.tsx
+++ src/app/presentation/9/page.tsx
@@ -1,27 +1,7 @@
-"use client";
-
-import { useSlide } from "@/lib/useSlide";
 import Points from "@/ui/Points";
-import Sources from "@/ui/Sources";
 
 const Slide = () => {
-  const { atleast } = useSlide(["definition", "professional", "proletarian"]);
-
-  return (
-    <>
-      <Points
-        points={[
-          "Il faut considérer son métier comme un métier",
-          atleast("professional") ? "C'est un professionnel" : null,
-          atleast("proletarian") ? "C'est un prolétaire" : null,
-        ]}
-      />
-
-      <Sources
-        sources={[atleast("proletarian") ? "Wiktionnaire - prolétaire" : null]}
-      />
-    </>
-  );
+  return <Points points={["Il faut considérer son métier comme un métier"]} />;
 };
 
 export default Slide;
